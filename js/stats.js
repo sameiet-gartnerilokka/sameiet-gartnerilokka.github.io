a6fed60eb3ed56f63b2b2db215c6e286547e30b4
DBV <daniel@micronarrativ.org>
2019-02-12 13:25:57 +0100

    General update

---
+++ js/stats.js
@@ -0,0 +1,19 @@
+<!-- Start Open Web Analytics Tracker -->
+<script type="text/javascript">
+//<![CDATA[
+var owa_baseUrl = 'https://stats.micronarrativ.org/';
+var owa_cmds = owa_cmds || [];
+owa_cmds.push(['setSiteId', 'b51476727190bc526572e13b20802e3e']);
+owa_cmds.push(['trackPageView']);
+owa_cmds.push(['trackClicks']);
+owa_cmds.push(['trackDomStream']);
+
+(function() {
+	var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+	owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+	_owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+	var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+}());
+//]]>
+</script>
+<!-- End Open Web Analytics Code -->
